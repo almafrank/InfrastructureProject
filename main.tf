e9e13f7c41616502f9b09f639a502c8017ff8bd4
almafrank <alma.k.frank@hotmail.com>
2025-03-17 12:42:31 +0100

    Update main.tf
    Added public subnet resource  to main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,13 @@ resource "aws_vpc" "TestVPC" {
         Name = "TestVPC"
     }
 }
+
+#4.Create a Subnet
+resource "aws_subnet" "subnet-1-public"{
+    vpc_id = aws_vpc.TestVPC.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+    Name = "SubNet 1 public"
+  }
+}
