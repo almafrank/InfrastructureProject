baebdf20b6099e5e0a8672f4c40a25d5b0043c7b
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-03-19 14:02:23 +0100

    changed cidblock to secuitygroup in private_ec2_security_group

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_security_group" "private_ec2_security_group" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks =  [aws_security_group.public_ec2_security_group.id]
+    security_groups =  [aws_security_group.public_ec2_security_group.id]
   }
 
   egress {
