efe84dd86c79765a47d3013f12d804f745027ad2
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-03-19 12:02:53 +0100

    added private subnet, its routing table and NAT gateway

--- main.tf
+++ main.tf
@@ -144,3 +144,43 @@ resource "aws_security_group" "public_ec2_security_group2" {
   }
 }
 
+#För privata instanser och resurser
+resource "aws_subnet" "subnet-2-private"{
+    vpc_id = aws_vpc.TestVPC.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+    Name = "SubNet 2 private"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.TestVPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+  depends_on = [aws_nat_gateway.nat]
+}
+
+#declare vpc for nat gateway
+resource "aws_eip" "nat" {
+  domain = "TestVPC"
+}
+#nat gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.subnet-1-public.id
+
+  depends_on = [aws_internet_gateway.gw]
+}
+#Create route table associations
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.subnet-2-private.id
+  route_table_id = aws_route_table.private_route_table.id
+}
