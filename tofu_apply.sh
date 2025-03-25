316bf5276ea416291a7a462cba9247f07ed5494c
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-03-25 13:35:26 +0100

    added tofu_apply.sh for getting the cloudshells ip automatically everytime we run tofu apply

---
+++ tofu_apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+
+my_ip=$(curl -s ifconfig.me)
+
+if [[ -z "$my_ip" ]]; then
+    echo "Failed to fetch IP. Exiting..."
+    exit 1
+fi
+
+sed -i "2s/.*/trusted_ips_for_ssh = [\"$my_ip\"]/" terraform.tfvars
+
+tofu apply
