8da3bff2a045734c477effef14bff2d7e751614a
isak0016 <isak999555@icloud.com>
2025-03-26 15:11:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,6 +62,6 @@ sed -i "s/\(ansible_ssh_common_args='-o ProxyJump=ec2-user@\)[0-9.]\+/\1$WEB1_PU
 
 echo "Ansible inventory file updated successfully!"
 
-ansible-playbook -i inventory python_setup.yml
-ansible-playbook -i inventory postgres_setup.yml
-ansible-playbook -i inventory app_setup.yml
+ansible-playbook -i inventory python_setup.yml -auto-approve
+ansible-playbook -i inventory postgres_setup.yml -auto-approve
+ansible-playbook -i inventory app_setup.yml -auto-approve
