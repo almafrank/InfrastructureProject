d596b27747ca14efec928576b38240696ecc20e8
isak0016 <isak999555@icloud.com>
2025-03-26 14:19:36 +0100

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
-#output "ec2_public_ip" {
-  #description = "Den publika IP-adressen för EC2-instansen"
-  #value       = aws_instance.webserver.public_ip
-#}
+output "public_Ec2_instance_public_ip" {
+  value = aws_instance.public_Ec2_instance.public_ip
+} 
+output "public_Ec2_instance2_public_ip" {
+  value = aws_instance.public_Ec2_instance2.public_ip
+}
+output "private_Ec2_instance_private_ip" {
+  value = aws_instance.private_Ec2_instance.private_ip
+}
